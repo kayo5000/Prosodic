152a00802fd15596c501e851da81ff7b366439ff
Kayo <khris.york@icloud.com>
2026-09-16 14:09:33 -0500

    chore: remove expo-doctor from verify script to fix Vercel builds

--- src/components/studio/paper/CadenceMarkdownEditor.tsx
+++ src/components/studio/paper/CadenceMarkdownEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import { View, TextInput, Text, StyleSheet, Platform, type NativeSyntheticEvent, type TextInputKeyPressEventData } from 'react-native';
 import { detectCrossBarWords } from '../../../services/cadenceDetectionEngine';
 
