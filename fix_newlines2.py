3ba71b2be138fb896122048c48b5bd20462676a3
Kayo <khris.york@icloud.com>
2026-09-17 06:11:58 -0500

    chore: remove temp scripts

--- fix_newlines2.py
+++
@@ -1,9 +0,0 @@
-
-with open('src/components/studio/SyllableInspectorModal.tsx', 'r') as f:
-    text = f.read()
-
-text = text.replace('\\\\n', '\\n')
-
-with open('src/components/studio/SyllableInspectorModal.tsx', 'w') as f:
-    f.write(text)
-
